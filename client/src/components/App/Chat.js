b7f568d67e17de0b055c60ff0e0c6c069cb9f080
dieDeMiguel <diedemiguel@gmail.com>
2021-04-15 18:30:28 +0200

    created chat component and gave basic styling

---
+++ client/src/components/App/Chat.js
@@ -0,0 +1,52 @@
+export default function Chat() {
+    return (
+        <div className="chat-wrapper">
+            <div className="centered-chat">
+                <div className="chat">
+                    <ul className="message-list">
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                        <li>
+                            This is a dummy message to display on the screen{" "}
+                        </li>
+                    </ul>
+                </div>
+                <form className="chat-form">
+                    <textarea></textarea>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    );
+}
