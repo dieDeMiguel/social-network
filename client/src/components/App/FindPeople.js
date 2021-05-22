8c2b78df22707543984e4094c7e8504668c76bde
dieDeMiguel <diedemiguel@gmail.com>
2021-05-22 12:48:24 +0200

    find users list imporved

--- client/src/components/App/FindPeople.js
+++ client/src/components/App/FindPeople.js
@@ -1,12 +1,10 @@
 import axios from "../../axios";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Search from "./Search";
+import { useState, useEffect } from "react";
 
 export default function FindPeople() {
     const [recentUsers, setRecentUsers] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
         axios.get("/users/most-recent").then((response) => {
@@ -14,131 +12,110 @@ export default function FindPeople() {
         });
     }, []);
 
-    useEffect(() => {
-        if (searchTerm.length <= 2) {
-            return;
-        }
-        axios
-            .get("/users/search", {
-                params: {
-                    q: searchTerm,
-                },
-            })
-            .then((response) => {
-                setSearchResults(response.data);
-            });
-    }, [searchTerm]);
-
-    function onChange(event) {
-        setSearchTerm(event.target.value);
-    }
-
     return (
         <>
-            <div className="overflow-x-auto">
-                <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
-                    <div className="w-full lg:w-5/6">
-                        <div className="bg-white shadow-md rounded my-6">
-                            <table className="min-w-max w-full table-auto">
-                                <thead>
-                                    <tr className="bg-green-300 text-gray-600 uppercase text-sm leading-normal">
-                                        <th className="py-3 px-6 text-left">
-                                            First Name
-                                        </th>
-                                        <th className="py-3 px-6 text-left">
-                                            Last Name
-                                        </th>
-                                        <th className="py-3 px-6 text-center">
-                                            Avatar
-                                        </th>
-                                        <th className="py-3 px-6 text-center">
-                                            Status
-                                        </th>
-                                        <th className="py-3 px-6 text-center">
-                                            Visit Profile
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="text-gray-600 text-sm font-light">
-                                    {recentUsers.map((user) => (
-                                        <tr
-                                            className="border-b border-gray-200 hover:bg-gray-100"
-                                            key={user.id}
-                                        >
-                                            <td className="py-3 px-6 text-left whitespace-nowrap text-center align-middle">
-                                                <div className="flex items-center">
-                                                    <div className="flex items-center justify-center">
-                                                        <img
-                                                            className="w-6 h-6 rounded-full border-gray-200 border transform hover:scale-125"
-                                                            src={"/logo.jpeg"}
-                                                        />
-                                                    </div>
-                                                    <span className="font-medium ml-2">
-                                                        {user.firstName}
-                                                    </span>
-                                                </div>
-                                            </td>
-                                            <td className="py-3 px-6  text-center align-middle">
-                                                <div className="flex items-center">
-                                                    <span>{user.lastName}</span>
-                                                </div>
-                                            </td>
-                                            <td className="py-3 px-6 text-center align-middle">
+            <div
+                className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden items-start friends-list"
+                style={{ alignItems: "flex-start", padding: "5%" }}
+            >
+                <div className="w-full lg:w-5/6">
+                    <div className="bg-white shadow-md rounded my-6">
+                        <table className="min-w-max w-full table-auto">
+                            <thead>
+                                <tr className="bg-green-300 text-gray-600 uppercase text-sm leading-normal">
+                                    <th className="py-3 px-6 text-left">
+                                        First Name
+                                    </th>
+                                    <th className="py-3 px-6 text-left">
+                                        Last Name
+                                    </th>
+                                    <th className="py-3 px-6 text-center">
+                                        Avatar
+                                    </th>
+                                    <th className="py-3 px-6 text-center">
+                                        Status
+                                    </th>
+                                    <th className="py-3 px-6 text-center">
+                                        Visit Profile
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="text-gray-600 text-sm font-light">
+                                {recentUsers.map((user) => (
+                                    <tr
+                                        className="border-b border-gray-200 hover:bg-gray-100"
+                                        key={user.id}
+                                    >
+                                        <td className="py-3 px-6 text-left whitespace-nowrap text-center align-middle">
+                                            <div className="flex items-center">
                                                 <div className="flex items-center justify-center">
                                                     <img
-                                                        className="w-12 h-12 rounded-full border-gray-200 border transform hover:scale-125 object-cover"
-                                                        src={
-                                                            user.profile_url ||
-                                                            "/avatar.png"
-                                                        }
+                                                        className="w-6 h-6 rounded-full border-gray-200 border transform hover:scale-125"
+                                                        src={"/logo.jpeg"}
                                                     />
                                                 </div>
-                                            </td>
-                                            <td className="py-3 px-6 text-center align-middle">
-                                                <span className="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">
-                                                    Active
+                                                <span className="font-medium ml-2">
+                                                    {user.firstName}
                                                 </span>
-                                            </td>
-                                            <td className="py-3 px-6 text-center align-middle">
-                                                <div className="flex item-center justify-center">
-                                                    <div className="w-8 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                                        <Link
-                                                            to={
-                                                                "/user/" +
-                                                                user.id
-                                                            }
+                                            </div>
+                                        </td>
+                                        <td className="py-3 px-6  text-center align-middle">
+                                            <div className="flex items-center">
+                                                <span>{user.lastName}</span>
+                                            </div>
+                                        </td>
+                                        <td className="py-3 px-6 text-center align-middle">
+                                            <div className="flex items-center justify-center">
+                                                <img
+                                                    className="w-12 h-12 rounded-full border-gray-200 border transform hover:scale-125 object-cover"
+                                                    src={
+                                                        user.profile_url ||
+                                                        "/avatar.png"
+                                                    }
+                                                />
+                                            </div>
+                                        </td>
+                                        <td className="py-3 px-6 text-center align-middle">
+                                            <span className="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">
+                                                Active
+                                            </span>
+                                        </td>
+                                        <td className="py-3 px-6 text-center align-middle">
+                                            <div className="flex item-center justify-center">
+                                                <div className="w-8 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                    <Link
+                                                        to={"/user/" + user.id}
+                                                    >
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke="currentColor"
                                                         >
-                                                            <svg
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                fill="none"
-                                                                viewBox="0 0 24 24"
-                                                                stroke="currentColor"
-                                                            >
-                                                                <path
-                                                                    strokeLinecap="round"
-                                                                    strokeLinejoin="round"
-                                                                    strokeWidth="2"
-                                                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                                                />
-                                                                <path
-                                                                    strokeLinecap="round"
-                                                                    strokeLinejoin="round"
-                                                                    strokeWidth="2"
-                                                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                                                                />
-                                                            </svg>
-                                                        </Link>
-                                                    </div>
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth="2"
+                                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                                            />
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth="2"
+                                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                                            />
+                                                        </svg>
+                                                    </Link>
                                                 </div>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
-                    <Search onSearchFieldChange={onChange} />
                 </div>
+                <Search />
             </div>
         </>
     );
--- client/src/components/App/Search.js
+++ client/src/components/App/Search.js
@@ -1,15 +1,86 @@
-export default function Search(onSearchFieldChange) {
+import { useState, useEffect } from "react";
+import axios from "../../axios";
+import { Link } from "react-router-dom";
+
+export default function Search() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        if (searchTerm.length <= 2) {
+            return;
+        }
+        axios
+            .get("/users/search", {
+                params: {
+                    q: searchTerm,
+                },
+            })
+            .then((response) => {
+                setSearchResults(response.data);
+            });
+    }, [searchTerm]);
+
+    function onChange(event) {
+        setSearchTerm(event.target.value);
+    }
     return (
-        <div>
-            <form onChange={onSearchFieldChange} className="m-4 flex">
-                <input
-                    className="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
-                    placeholder="Search for other users..."
-                />
-                <button className="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">
-                    Search
-                </button>
-            </form>
+        <div className="flex flex-col">
+            <div>
+                <form
+                    onChange={onChange}
+                    className="flex border border-black rounded-lg mt-12 ml-6"
+                >
+                    <input
+                        className="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white text-xs p-4"
+                        placeholder="Search other users..."
+                    />
+                    <button className="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">
+                        Search
+                    </button>
+                </form>
+            </div>
+            <div>
+                <div className="container mb-2 flex mx-auto w-full items-center justify-center">
+                    <ul className="flex flex-col p-4">
+                        {searchResults.map((user) => (
+                            <li
+                                className="border-gray-400 flex flex-row"
+                                key={user.id}
+                            >
+                                <div className="select-none mb-4 flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-2 rounded-2xl border-2 hover:shadow-2xl border-red-400">
+                                    <div className="flex-1 pl-1">
+                                        <div className="font-medium">
+                                            <p
+                                                style={{
+                                                    width: "100px",
+                                                    height: "100px",
+                                                }}
+                                            >
+                                                <Link to={"/user/" + user.id}>
+                                                    <img
+                                                        className="profile-picture"
+                                                        src={user.profile_url}
+                                                        style={{
+                                                            borderRadius: "5px",
+                                                            objectFit: "cover",
+                                                            width: "100px",
+                                                            height: "100px",
+                                                        }}
+                                                    />
+                                                </Link>
+                                            </p>
+                                            <p>
+                                                {user.firstName} {user.lastName}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
